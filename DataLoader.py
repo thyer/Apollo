e897ee83c26887b2e389fb003bbd5e3e942dbd31
Trenton Adam Hyer <thyer@byu.edu>
2016-01-04 16:37:54 -0700

    Generalized article entry

--- DataLoader.py
+++ DataLoader.py
@@ -1,9 +1,25 @@
 import sqlite3
+import sys
+
 conn = sqlite3.connect('content.db')
 
 c = conn.cursor()
 
+c.execute('''DROP TABLE articles''')
 c.execute('''CREATE TABLE IF NOT EXISTS articles (date text, topic text, title text, author text, link text)''')
 
-c.execute("INSERT INTO articles VALUES('2015-12-17', 'INFOSEC', 'Social Engineering in the 21st Century', 'Brian Krebs', 'https://krebsonsecurity.com')")
+num_lines = sum(1 for line in open('example_articles.txt'))
+if num_lines%5 != 0:
+    conn.rollback()
+    sys.exit("ERROR: Bad input file!")
+with open('example_articles.txt') as f:
+    for x in range(int(num_lines/5)):
+        date = f.readline()
+        topic = f.readline()
+        title = f.readline()
+        author = f.readline()
+        link = f.readline()
+        article = (date, topic, title, author, link)
+        c.execute("INSERT INTO articles VALUES (?, ?, ?, ?, ?) ", article)
+
 conn.commit()
